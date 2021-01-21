a29db6d90ccbcf96ba563f29f501c3d23a50c0a6
Jed <59712443+jedvod@users.noreply.github.com>
2021-01-21 23:27:50 +0000

    Update base.sql

--- base.sql
+++ base.sql
@@ -1,25 +1,7 @@
-#DROP TABLE IF EXISTS smtp_logs;
-CREATE TABLE  IF NOT EXISTS smtp_logs (
-         id int(11) NOT NULL auto_increment,
-	 queue varchar(12) NOT NULL,
-	 date date NOT NULL,
-	 time time NOT NULL,
-	 email_from varchar(200) NOT NULL,
-	 email_to varchar(200) NOT NULL,
-	 size int(20) default NULL,
-	 status varchar(20) default NULL,
-	 status_comment varchar(200) default NULL,
-         PRIMARY KEY  (id),
-	 INDEX (date),
-	 INDEX (email_from, email_to))  ENGINE=InnoDB;
-DROP TABLE IF EXISTS noqueue_logs;
-CREATE TABLE IF NOT EXISTS noqueue_logs (
-	 id int(11) NOT NULL auto_increment,
-	 date date NOT NULL,
-	 time time NOT NULL,
-	 email_from varchar(200) NOT NULL,
-	 email_to varchar(200) NOT NULL,
-	 error_msg varchar(200) NOT NULL,
-	 PRIMARY KEY (id),
-	 INDEX (date),
-	 INDEX (email_from, email_to)) ENGINE=InnoDB;
+#DROP TABLE IF EXISTS roundcube_logs;
+CREATE TABLE  IF NOT EXISTS roundcube_logs (
+	id int(11) NOT NULL auto_increment,
+	date varchar(10) NOT NULL,
+	time varchar(8) NOT NULL,
+	log text
+	PRIMARY KEY (ID))  ENGINE=InnoDB;
